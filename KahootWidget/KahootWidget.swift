af0b4f29717dba5f8a4a28d0787ecfd4c5c115c6
Andreas Schjønhaug <andreas@schjonhaug.com>
2020-09-18 13:41:34 +0200

    Added dummy image

---
+++ KahootWidget/KahootWidget.swift
@@ -0,0 +1,71 @@
+//
+//  KahootWidget.swift
+//  KahootWidget
+//
+//  Created by Andreas Schjønhaug on 18/09/2020.
+//
+
+import WidgetKit
+import SwiftUI
+
+struct Provider: TimelineProvider {
+    func placeholder(in context: Context) -> SimpleEntry {
+        SimpleEntry(date: Date())
+    }
+
+    func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
+        let entry = SimpleEntry(date: Date())
+        completion(entry)
+    }
+
+    func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
+        var entries: [SimpleEntry] = []
+
+        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
+        let currentDate = Date()
+        for hourOffset in 0 ..< 5 {
+            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
+            let entry = SimpleEntry(date: entryDate)
+            entries.append(entry)
+        }
+
+        let timeline = Timeline(entries: entries, policy: .atEnd)
+        completion(timeline)
+    }
+}
+
+struct SimpleEntry: TimelineEntry {
+    let date: Date
+}
+
+struct KahootWidgetEntryView : View {
+    var entry: Provider.Entry
+
+    var body: some View {
+        VStack {
+            Image("DummyImage")
+            Text("TOP PICKS")
+            Text("World architecture")
+        }
+    }
+}
+
+@main
+struct KahootWidget: Widget {
+    let kind: String = "KahootWidget"
+
+    var body: some WidgetConfiguration {
+        StaticConfiguration(kind: kind, provider: Provider()) { entry in
+            KahootWidgetEntryView(entry: entry)
+        }
+        .configurationDisplayName("My Widget")
+        .description("This is an example widget.")
+    }
+}
+
+struct KahootWidget_Previews: PreviewProvider {
+    static var previews: some View {
+        KahootWidgetEntryView(entry: SimpleEntry(date: Date()))
+            .previewContext(WidgetPreviewContext(family: .systemSmall))
+    }
+}
