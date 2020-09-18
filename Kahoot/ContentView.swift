1f63b4838345649cb64648919c9eff9449c3b379
Andreas Schjønhaug <andreas@schjonhaug.com>
2020-09-18 13:21:13 +0200

    Initial Commit

---
+++ Kahoot/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Kahoot
+//
+//  Created by Andreas Schjønhaug on 18/09/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Kahoot/KahootApp.swift
@@ -0,0 +1,17 @@
+//
+//  KahootApp.swift
+//  Kahoot
+//
+//  Created by Andreas Schjønhaug on 18/09/2020.
+//
+
+import SwiftUI
+
+@main
+struct KahootApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
